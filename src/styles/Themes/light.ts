0a67c6c19d9e4c85bdc8120c432fced488f2bf6d
AdrianaIto <adriana@sbworks.jp>
2021-08-26 11:40:27 +0900

    add dark and light mode style

---
+++ src/styles/Themes/light.ts
@@ -0,0 +1,13 @@
+const light = {
+    title: 'light',
+
+    colors: {
+        primary: '#C62E65',
+        secondary: '#D63AF9',
+
+        background: '#F5F5F5',
+        text: '#333'
+    }
+}
+
+export const lightTheme = { ...light }
