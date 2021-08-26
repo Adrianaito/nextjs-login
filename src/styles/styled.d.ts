25c731c9cc2650fe3ce4d55db9fb85ecc6259f1e
AdrianaIto <adriana@sbworks.jp>
2021-08-26 11:43:18 +0900

    add theme interface

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -8,3 +8,16 @@ export type Theme = typeof theme
 declare module 'styled-components' {
     export interface DefaultTheme extends Theme { }
 }
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        title: string,
+
+        colors: {
+            primary: string,
+            secondary: string,
+
+            background: string,
+            text: string
+        }
+    }
+}
