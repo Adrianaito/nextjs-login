63267fdf0100ff34ba7db5a8d532fd921bd0a580
AdrianaIto <adriana@sbworks.jp>
2021-08-26 11:43:46 +0900

    apply theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,49 @@
-import { AppProps } from 'next/dist/shared/lib/router/router'
-import GlobalStyle from '../styles/global'
 import { ThemeProvider } from 'styled-components'
-import theme from '../styles/theme'
+import React, { useEffect, useState } from 'react'
+import usePersistedState from '../utils/usePersistedState'
+import useDarkMode from 'use-dark-mode'
+
+import GlobalStyle from '../styles/global'
+import Header from '../components/Header'
+
+import { lightTheme } from '../styles/Themes/light'
+import { darkTheme } from '../styles/Themes/dark'
 
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+
+const MyApp = ({ Component, pageProps }) => {
+
+    const [theme, setTheme] = usePersistedState('theme', lightTheme);
+
+    const [isMounted, setIsMounted] = useState(false)
+
+    // FOR USE DARK MODE LIBRARY
+    // const darkMode = useDarkMode(true)
+    // const theme = darkMode.value ? dark : light
+
+    useEffect(() => {
+        setIsMounted(true)
+        // localStorage.getItem("darkMode")
+        // cookie.get("theme")
+    }, [])
+
+    const toggleTheme = () => {
+        setTheme(theme.title == "light" ? darkTheme : lightTheme);
+        // darkMode.toggle()
+    }
     return (
         <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
+
+            {isMounted &&
+                <>
+                    <Header toggleTheme={toggleTheme} />
+                    <Component {...pageProps} />
+                </>
+            }
             <GlobalStyle />
         </ThemeProvider>
     )
-}
 
+}
 export default MyApp
+
