8313ab0539dd04288606a2bad28a0345a15a6429
AdrianaIto <adriana@sbworks.jp>
2021-08-31 17:14:46 +0900

    add sign in form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import Head from 'next/head'
+import { useForm } from 'react-hook-form'
+
 import { Container } from '../styles/pages/Home'
+import { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+import Button from "../components/Button"
 
 const Home: React.FC = () => {
+
+    const { register, handleSubmit } = useForm();
+    const { signIn } = useContext(AuthContext)
+
+    async function handleSignIn(data) {
+        await signIn(data)
+    }
+
+
     return (
         <Container>
             <Head>
@@ -11,6 +25,74 @@ const Home: React.FC = () => {
                 <h1>
                     Hello!
                 </h1>
+                <div className="max-w-sm w-full space-y-8">
+                    <div>
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+                    </div>
+                    <form className="mt-8 space-y-6" onSubmit={handleSubmit(handleSignIn)}>
+                        <input type="hidden" name="remember" defaultValue="true" />
+                        <div className="rounded-md shadow-sm -space-y-px">
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email address
+                                </label>
+                                <input
+                                    {...register('email')}
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Email address"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only">
+                                    Password
+                                </label>
+                                <input
+                                    {...register('password')}
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Password"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-between">
+                            {/* <div className="flex items-center">
+                                <input
+                                    id="remember_me"
+                                    name="remember_me"
+                                    type="checkbox"
+                                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                                />
+                                <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+                                    Remember me
+                                </label>
+                            </div>
+
+                            <div className="text-sm">
+                                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                    Forgot your password?
+                                </a>
+                            </div> */}
+                        </div>
+
+                        <div>
+                            <Button
+                                type="submit"
+                            >
+                                Sign in
+                            </Button>
+                        </div>
+                    </form>
+                </div>
             </main>
         </Container>
     )
